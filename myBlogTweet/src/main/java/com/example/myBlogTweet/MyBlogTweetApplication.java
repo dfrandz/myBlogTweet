f8216095babec39f34f32b6492ee84841c80e6bc
dfrandz <dfrandz8@gmail.com>
2023-01-06 18:52:00 +0100

    initalize project

---
+++ myBlogTweet/src/main/java/com/example/myBlogTweet/MyBlogTweetApplication.java
@@ -0,0 +1,13 @@
+package com.example.myBlogTweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogTweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogTweetApplication.class, args);
+	}
+
+}
